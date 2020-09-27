1fc0d9ae794cee1ea83e9b459b408d76088a8426
Sean McFarlane <seanmcfarlane115@gmail.com>
2020-09-27 11:32:44 -0700

    Initial commit

---
+++ Editor/SpriteSwapperHandler.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+using Object = UnityEngine.Object;
+
+[CustomEditor(typeof(SpriteSwapper), true)]
+public class SpriteSwapperHandler : OdinEditor {
+
+    private SpriteSwapper mySpriteSwapper;
+
+	void Awake() {
+		mySpriteSwapper = (SpriteSwapper)target;
+	}
+
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		if(GUILayout.Button("Populate")) {
+			Populate();
+		}
+	}
+
+	public void Populate() {
+
+		mySpriteSwapper.swapSets.Clear();
+		Debug.Log("Fetching all assets at path: " + mySpriteSwapper.originalPath);
+
+		Object[] originalAssets = GetAtPath(mySpriteSwapper.originalPath);
+		List<Sprite> originalSprites = new List<Sprite>();
+		List<Texture2D> originalTextures = new List<Texture2D>();
+		foreach(Object obj in originalAssets) {
+			if(obj is Sprite) {
+				originalSprites.Add(obj as Sprite);
+			}
+			if(obj is Texture2D) {
+				originalTextures.Add(obj as Texture2D);
+			}
+		}
+
+		Object[] variantAssets = GetAtPath(mySpriteSwapper.variantPath);
+		List<Sprite> variantSprites = new List<Sprite>();
+		foreach(Object obj in variantAssets) {
+			if(obj is Sprite) {
+				variantSprites.Add(obj as Sprite);
+			}
+		}
+
+		Debug.Log("Got " + originalAssets.Length + " objects.");
+		Debug.Log("Got " + originalSprites.Count + " sprites.");
+		Debug.Log("Got " + originalTextures.Count + " textures.");
+		Debug.Log("Got " + variantSprites.Count + " variant sprites.");
+
+		foreach(Texture2D tex in originalTextures) {
+			Debug.Log("Processing texture:" + tex.name);
+			SwapSet swapSet = new SwapSet(tex);
+			List<Sprite> spriteList = new List<Sprite>();
+			foreach(Sprite sp in originalSprites) {
+				if(swapSet.isPartOfSet(sp)) {
+					spriteList.Add(sp);
+				}
+			}
+			spriteList.Sort(CompareSprites); // Ensure sprites are sequenced.
+			swapSet.original = spriteList.ToArray();
+
+			List<Sprite> variantList = new List<Sprite>();
+			foreach(Sprite sp in variantSprites) {
+				if(swapSet.isPartOfSet(sp)) {
+					variantList.Add(sp);
+				}
+			}
+			variantList.Sort(CompareSprites); // Ensure sprites are sequenced.
+			swapSet.variant = variantList.ToArray();
+
+			mySpriteSwapper.swapSets.Add(swapSet);
+		}
+	}
+
+	private static int CompareSprites(Sprite x, Sprite y) {
+		int xIndex = x.name.LastIndexOf("_");
+		int yIndex = y.name.LastIndexOf("_");
+
+		//string xs = x.name.Substring(xIndex+1);
+		//string ys = y.name.Substring(yIndex+1);
+
+		//Debug.Log("xs=" + xs + ", ys=" + ys);
+
+		int xVal = Convert.ToInt32(x.name.Substring(xIndex + 1));
+		int yVal = Convert.ToInt32(y.name.Substring(yIndex + 1));
+		return xVal - yVal;// x.name.CompareTo(y.name);	
+	}
+
+
+	public static Object[] GetAtPath(string path) {
+		//Removing the excess /Assets of the datapath.
+		string dataPath = Application.dataPath;
+		int index = dataPath.LastIndexOf("/");
+		if(index > 0)
+			dataPath = dataPath.Substring(0, index);
+
+		string[] fileEntries = Directory.GetFiles(dataPath + "/" + path);
+
+		List<Object> al = new List<Object>();
+		foreach(string fileName in fileEntries) {
+			string lastPart = fileName;
+			lastPart = lastPart.Replace(@"/", @"\");
+			index = lastPart.LastIndexOf(@"\");
+			lastPart = lastPart.Substring(index);
+			string localPath = path;
+
+			if(index > 0)
+				localPath += lastPart;
+
+			localPath = localPath.Replace(@"/", @"\");
+			//Debug.Log("localpath:" + localPath);
+
+			Object[] t = AssetDatabase.LoadAllAssetsAtPath(localPath);
+			foreach(Object obj in t) {
+				if(t != null) {
+					//Debug.Log("obj:" + obj.name);
+					al.Add(obj);
+				}
+			}
+		}
+		return al.ToArray();
+	}
+
+}
---
+++ Scripts/SpriteSwapper.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+
+[AddComponentMenu("Fulcrum/Anim/SpriteSwapper")]
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteSwapper : SerializedMonoBehaviour {
+
+	[SerializeField]
+	[TableList(DrawScrollView = true, MaxScrollViewHeight = 200, MinScrollViewHeight = 100)]
+	public List<SwapSet> swapSets = new List<SwapSet>();
+
+	[SerializeField, FolderPath]
+	public string originalPath;
+	[SerializeField, FolderPath]
+	public string variantPath;
+
+
+	SpriteRenderer sr;
+	private void Awake() {
+		sr = this.GetComponent<SpriteRenderer>();
+	}
+
+	private void LateUpdate() {
+		for(int i = 0; i < swapSets.Count; i++) {
+			if(sr.sprite.texture == swapSets[i].tex) {
+				for(int j = 0; j < swapSets[i].original.Length; j++) {
+					if(sr.sprite == swapSets[i].original[j] && swapSets[i].variant.Length > 0) {
+						int index = j % swapSets[i].variant.Length;
+						sr.sprite = swapSets[i].variant[index];
+					}
+				}
+			}
+		}
+	}
+
+
+
+}
+
+[Serializable]
+public class SwapSet {
+
+	//[TableColumnWidth(57, Resizable = true)]
+	[ReadOnly]
+	public string name;
+
+	[PreviewField(64, ObjectFieldAlignment.Center), TableColumnWidth(64, Resizable = false)]
+	public Texture2D tex;
+
+	//[AssetList(CustomFilterMethod = "isPartOfSet", Path = @"Images/Platformer/")]
+	public Sprite[] original;
+
+	public Sprite[] variant;
+
+	public SwapSet(Texture2D texture) {
+		tex = texture;
+		name = texture.name;
+	}
+
+	public bool isPartOfSet(Sprite sp) {
+		return sp.texture.name == name;
+	}
+}
+
